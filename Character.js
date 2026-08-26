19cfd14eb45902c589950c1d00221d208a69b7c4
Tauane <tauanesuelensenac@gmail.com>
2026-08-26 20:40:00 -0300

    Add files via upload

---
+++ Character.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Character = void 0;
+class Character {
+    name;
+    level;
+    health;
+    constructor(name, level, health) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+    }
+    showInfo() {
+        console.log(`========================
+Character
+========================
+Name: ${this.name}
+Level: ${this.level}
+Health: ${this.health}`);
+    }
+    getName() {
+        return this.name;
+    }
+    getLevel() {
+        return this.level;
+    }
+    getHealth() {
+        return this.health;
+    }
+    setName(name) {
+        this.name = name;
+    }
+    setLevel(level) {
+        this.level = level;
+    }
+    setHealth() {
+        this.health -= 0 ? 0 : this.health;
+    }
+    takeDamage(amount) {
+        this.health -= amount;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+}
+exports.Character = Character;
